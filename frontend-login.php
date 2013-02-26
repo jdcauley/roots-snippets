5bd350b4ac76858461a0b5741f07708d7584757d
Jordan Cauley <jordan@cauley.co>
2013-02-26 14:08:57 -0500

    Update frontend-login.php

--- frontend-login.php
+++ frontend-login.php
@@ -1,10 +1,12 @@
+// Frontend Login and Logout for Roots
+
 <?php if (is_user_logged_in()) : ?>
      
-     <a button class="btn pull-right" href="<?php echo wp_logout_url(); ?>">Logout</a>
+     <a button class="btn" href="<?php echo wp_logout_url(); ?>">Logout</a>
   
      <?php else : ?>
  
-        <form class="form-inline pull-right" name="loginform" id="loginform" action="/wp-login.php" method="post" style="margin: 0px" />
+        <form class="form-inline" name="loginform" id="loginform" action="/wp-login.php" method="post" style="margin: 0px" />
             <input type="text" name="log" id="user_login" class="input-medium" value="" tabindex="10" placeholder="Username" required="required" />
             <input type="password" name="pwd" id="user_pass" class="input-medium" value="" tabindex="20" placeholder="Password" required="required" />
             <input type="submit" name="wp-submit" id="wp-submit" class="btn-primary" value="Log In" tabindex="100" />
