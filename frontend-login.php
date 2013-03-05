e827cc9d3fe4fc51c1d3f414d2c228c14dfb8d8c
Jordan Cauley <jordan@cauley.co>
2013-03-05 07:38:35 -0500

    Implement Suggested Edits
    Edits as suggested in the following comment:
    
    https://github.com/retlehs/roots-snippets/pull/1#issuecomment-14423094

--- frontend-login.php
+++ frontend-login.php
@@ -1,17 +1,21 @@
-// Frontend Login and Logout for Roots
+<?php
 
-<?php if (is_user_logged_in()) : ?>
-     
-     <a button class="btn" href="<?php echo wp_logout_url(); ?>">Logout</a>
-  
-     <?php else : ?>
- 
-        <form class="form-inline" name="loginform" id="loginform" action="/wp-login.php" method="post" style="margin: 0px" />
-            <input type="text" name="log" id="user_login" class="input-medium" value="" tabindex="10" placeholder="Username" required="required" />
-            <input type="password" name="pwd" id="user_pass" class="input-medium" value="" tabindex="20" placeholder="Password" required="required" />
-            <input type="submit" name="wp-submit" id="wp-submit" class="btn-primary" value="Log In" tabindex="100" />
-            <input type="hidden" name="redirect_to" value="/" />
-            <input type="hidden" name="testcookie" value="1" />
-        </form>
+/**
+ * Front-end login/logout
+ */
+
+if (is_user_logged_in()) : ?>
+
+  <a button class="btn" href="<?php echo wp_logout_url(); ?>">Logout</a>
+
+<?php else : ?>
+
+  <form class="form-inline" name="loginform" id="loginform" action="<?php echo home_url(); ?>/wp-login.php" method="post">
+    <input type="text" name="log" id="user_login" class="input-medium" value="" tabindex="10" placeholder="Username">
+    <input type="password" name="pwd" id="user_pass" class="input-medium" value="" tabindex="20" placeholder="Password">
+    <input type="submit" name="wp-submit" id="wp-submit" class="btn btn-primary" value="Log In">
+    <input type="hidden" name="redirect_to" value="<?php echo home_url(); ?>">
+    <input type="hidden" name="testcookie" value="1">
+  </form>
    
 <?php endif; ?>
