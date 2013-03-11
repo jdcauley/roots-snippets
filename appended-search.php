5247ae6a2f44b28737c359fac19e6af0123e24b9
Jordan Cauley <jordan@cauley.co>
2013-03-11 16:25:41 -0300

    Create appended-search.php

---
+++ appended-search.php
@@ -0,0 +1,10 @@
+/**
+ * Search with Appended Button
+ */
+
+<form role="search" method="get" id="searchform" class="form-search" action="<?php echo home_url('/'); ?>">
+  <div class="input-append">
+    <input type="text" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="search-query" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
+    <button type="submit" id="searchsubmit" value="<?php _e('Search', 'roots'); ?>" class="btn"><i class="icon-search"></i></button>
+  </div>
+</form>
